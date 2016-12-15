8f4b58afac5000ecc17a641ed263de836241b14a
Zach Holman <zach@zachholman.com>
2016-12-14 16:12:43 -0800

    remove ruby version from prompt
    Ruby versions don't really matter anymore, now that we're in a post 1.9
    world. 💖

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -82,7 +82,7 @@ battery_status() {
   $ZSH/bin/battery-status
 }
 
-export PROMPT=$'\n$(battery_status) $(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
 }
